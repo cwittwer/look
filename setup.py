97fc501c56799450bf658b5dde41643d4522a7a8
Carson Wittwer <35505233+cwittwer@users.noreply.github.com>
2023-03-28 14:17:02 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python-look",
+    version="0.1.0",
+	license="MIT",
+    author="Carson Wittwer",
+    author_email="wittwer.carson@googlemail.com",
+    description="Simple wrapper code to run inference on images using the model and code from the Looking Repo from VITA-EPFL.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cwittwer/look",
+    packages=find_packages(exclude=("tests", "requirements.txt",)),
+	include_package_data=True,
+	install_requires=[
+    "tqdm",
+    "numpy",
+    "opencv_python==4.5.1.48",
+    "seaborn",
+    "matplotlib",
+    "torch",
+    "torchvision",
+    "openpifpaf==0.13.8",
+    "scikit_image==0.17.2",
+    "Pillow",
+    "scikit_learn"
+	],
+    classifiers=[
+      "Development Status :: 4 - Beta",
+		  "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python :: 3.6",
+		  "Programming Language :: Python :: 3.7",
+		  "Programming Language :: Python :: 3.8",
+		  "Programming Language :: Python :: 3.9"
+    ],
+    python_requires='>=3.6',
+    python_requires='<=3.9',
+)
