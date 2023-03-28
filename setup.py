288f576a24738c46775d7905c1196330f8ec4ac3
Carson Wittwer <wittwer.carson@vt.edu>
2023-03-28 14:26:38 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -38,6 +38,5 @@ setup(
 		  "Programming Language :: Python :: 3.8",
 		  "Programming Language :: Python :: 3.9"
     ],
-    python_requires='>=3.6',
-    python_requires='<=3.9',
+    python_requires='>=3.6.9, <=3.9'
 )
